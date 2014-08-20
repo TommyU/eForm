84f26db2f378e64991983e57df635b87f74a149f
TommyU <lion_awake@163.com>
2014-08-20 10:37:03 +0800

    add route

---
+++ urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import patterns, include, url
+
+from views import *
+
+urlpatterns = patterns('',
+    url(r'^$',requestList.as_view()),
+    url(r'^(?P<pk>[\d]+)/$',requestDetail.as_view()),
+    url(r'^create/$',requestCreate.as_view()),
+    url(r'^delete/(?P<pk>[\d]+)$',requestDelete.as_view()),
+    url(r'^update/(?P<pk>[\d]+)$',requestUpdate.as_view()),
+
+)
